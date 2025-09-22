e208bec61cead9986c7cedef9beff0b75c9cbced
Madhanrajoy <madhanraj@kleidsys.com>
2025-09-22 11:08:59 +0530

    erp 1st push

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const cors = require('cors');
-
 app.use(cors());
-
-const dbConnect = require('./config/db');
-
-
 const dotenv = require('dotenv');
 const path = require('path');
 // const { env } = require('process');0
 dotenv.config({path:path.join(__dirname, ".env")});
+
+const corsOptions ={
+    origin:[process.env.VITE_URL , `http://localhost:3000`],
+    methods:['GET','POST','DELETE','PUT'],
+    Credentials:true
+};
+app.use(cors(corsOptions));
+
+const dbConnect = require('./config/db');
+
+
  
-
-
 const approval = require('./routes/approval.js');
 const { getSalesOrderData,getSalesOrderById} = require('./routes/salesOrderReport.js');
 const approvalCount = require('./routes/approvalCounts.js');
@@ -44,25 +48,20 @@ app.use('/api',prdStatus);
 
 // app.use('/api/salesreport',salesOrderReport);
 
+// Serve static files from React build folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Catch-all handler to send React's index.html for any request not handled by API
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 const PORT = process.env.DB_PORT;
 const NODE_ENV = process.env.NODE_ENV;
 app.listen(PORT ,'0.0.0.0' ,() =>{
     console.log(`server listening to ${PORT} AT ${NODE_ENV}`);
     
-})
-
-// const express = require('express');
-// const cors = require('cors');
-// const { getSalesOrderData } = require('./routes/salesOrderReport');
-
-// require('dotenv').config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/api/sales-order', getSalesOrderData);
-
-// const PORT = process.env.DB_PORT || 3001;
-// app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+});
